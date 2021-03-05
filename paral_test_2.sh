7517e1f2cbbaf74dbb2643e48da05d08b81cf510
Gustavo Hernandez Mejia <gustavohdezm@gmail.com>
2021-03-05 21:07:42 +0100

    sh_2

---
+++ paral_test_2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+#SBATCH --nodes=1                   # the number of nodes you want to reserve
+#SBATCH --ntasks-per-node=36        # the number of CPU cores per node
+#SBATCH --partition=normal          # on which partition to submit the job
+#SBATCH --time=1:00:00              # the max wallclock time (time limit your job will run)
+ 
+#SBATCH --job-name=GHM_test         # the name of your job
+#SBATCH --mail-type=ALL             # receive an email when your job starts, finishes normally or is aborted
+#SBATCH --mail-user=mejia@uni-muenster.de # your mail address
+
+python test_1_main.py
+
+# LOAD MODULES HERE IF REQUIRED
+
+# START THE APPLICATION
